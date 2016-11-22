63b13e654741fb4880d9f50c7314bb162f0fc765
Антон Голомазов <golomazov-tosha2012@yandex.ru>
2016-11-22 23:29:14 +0300

    Initial repo

---
+++ src/constant.js
@@ -0,0 +1,1 @@
+module.exports = 'Application init string!!';
---
+++ src/index.js
@@ -0,0 +1,14 @@
+var $ = require('jquery');
+var str = require('./constant');
+
+var App = function() {
+    var htmlstr = str;
+    $('body').prepend('<h1>' + htmlstr + '!!!</h1>');
+};
+
+App.prototype.run = function() {
+    $('body').prepend('<h1>Run application</h1>');
+};
+
+var app = new App();
+app.run();
